79d9a3c9b7ebe6a3778a2f51e949b1f3987ba268
singhakash7123 <vivek414viveksingh@gmail.com>
2025-08-29 16:06:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,127 @@
-let Meals = document.querySelector('.meals') ;
-console.log(Meals);
-let Input = document.querySelector('.search-box') ;
-let search_btn = document.querySelector('.search-btn');
-console.log(Input);
-console.log(search_btn);
+let input = document.querySelector('.inputbar')
+let Search = document.querySelector('.input-btn')
+let result = document.querySelector('.results')
 
-search_btn.addEventListener('click', async()=>{
-      while(Meals.firstChild){
-            Meals.removeChild(Meals.firstChild)   
-          }
-    let value = Input.value ;
-    await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${value}`)
-    .then((res) =>{
-        return res.json()
-    })
-    .then((data)=>{
-       let arr = data.meals ;
-       console.log(arr);
-       arr.forEach(ele => {
-        let Child =  document.createElement('div') ;
-   let image =  document.createElement('img') ;
-   let H3 =  document.createElement('h3') ;
-   let Button =  document.createElement('button') ;
-   Button.innerText = 'Get Recipe' ;
-   Child.appendChild(image)
-   Child.appendChild(H3)
-   Child.appendChild(Button)
-   H3.innerText =`${ele.strMeal}`
-   image.src =`${ele.strMealThumb}`
-   Meals.appendChild(Child) ;
-   Child.classList.add('cards')
-       });
+
+
+console.log(input);
+console.log(Search);
+
+
+
+
+   
+    Search.addEventListener('click',async ()=>{
        
-    })
+         if(input.value === ''){
+    alert('write the ingredient')
+}
+else{
+  while(result.firstChild){
+            result.removeChild(result.firstChild)   
+          }
+           let value = input.value ;
+         await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${value}`)
+         .then(res=>res.json())
+         .then(data=>{
+          console.log(data.meals)
+           data.meals.forEach(element => {
+            result.innerHTML +=` <div class="card">
+          <img src=${element.strMealThumb} class="image">
+          <div class="details">
+            <h3 class="info name">${element.strMeal}</h3>
+            <button class="info info-btn">Get Recipe</button>
+          </div>
+           <div class="ingredient"></div>
+        </div>`
 
-   
-   
-})
+          })
+         let get =  document.querySelectorAll('.info-btn')
+         get.forEach((ele)=>{
+          ele.addEventListener('click',async ()=>{
+                     let x = ele.parentNode.childNodes[1].innerHTML
+                    let ingre = ele.parentNode.parentNode.childNodes[5]
+    
+                  ingre.addEventListener('mouseout',()=>[
+                    ingre.innerHTML = '' 
+                   ])
+                   console.log(ingre);
+                     await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${x}`)
+                     .then(res=>res.json())
+                     .then(data=>{
+                       let obj = data.meals[0]
+                       for(key in obj){
+                         if(key.startsWith('strIngredient')&& obj[key]){
+                           ingre.innerHTML += `${obj[key]} + <br>`
+                         }
+                       }
+                     })
+          })
+         })
+          
+
+         }
+          )
+    //        document.addEventListener('click',async (e)=>{
+    //   let x = e.target.parentNode.childNodes[1].innerHTML
+    // let ingre = e.target.parentNode.parentNode.childNodes[5]
+    // ingre.addEventListener('mouseout',()=>[
+    //   ingre.innerHTML = '' 
+    // ])
+    // console.log(ingre);
+    //   await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${x}`)
+    //   .then(res=>res.json())
+    //   .then(data=>{
+    //     let obj = data.meals[0]
+    //     for(key in obj){
+    //       if(key.startsWith('strIngredient')&& obj[key]){
+    //         ingre.innerHTML += `${obj[key]} + <br>`
+    //       }
+    //     }
+    //   })
+      
+    //  }) 
+     
+
+          
+          
+}
+
+    //  document.addEventListener('click',async (e)=>{
+    //   let x = e.target.parentNode.childNodes[1].innerHTML
+    // let ingre = e.target.parentNode.parentNode.childNodes[5]
+    // ingre.addEventListener('mouseout',()=>[
+    //   ingre.innerHTML = '' 
+    // ])
+    // console.log(ingre);
+    //   await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${x}`)
+    //   .then(res=>res.json())
+    //   .then(data=>{
+    //     let obj = data.meals[0]
+    //     for(key in obj){
+    //       if(key.startsWith('strIngredient')&& obj[key]){
+    //         ingre.innerHTML += `${obj[key]} + <br>`
+    //       }
+    //     }
+    //   })
+      
+    //  })
+          
+
+
+})    
+
+         
+
+        
+
+         
+         
+        
+
+        
+
+        
+
+    
+
